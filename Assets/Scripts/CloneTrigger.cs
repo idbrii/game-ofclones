21ca7308674273c646e8f5878504fb9a8a98806e
David Briscoe <idbrii@gmail.com>
2014-08-29 22:51:46 -0700

    Create and spawn a clone
    Create a simple clone as a copy of hero. Remove unnecessary components
    and give it an empty CloneControl.
    
    Create a spawner that creates a clone at a location when you enter its
    collision trigger.
    
    Creates clone and cloneTrigger prefabs that I think I've setup
    correctly. I did new prefab in Project, new GameObject in Hierarchy,
    drag the GameObject to the prefab in Project, then delete the original
    GameObject (at least for clone -- since I'm spawning them I don't want
    one in my scene).

---
+++ Assets/Scripts/CloneTrigger.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloneTrigger : MonoBehaviour
+{
+	[Tooltip("The object to create when this trigger is touched.")]
+	public GameObject archetype;
+
+	[Tooltip("Seconds between subsequent clones.")]
+	public float cooldownTime = 1f;
+
+	// When we last created a clone.
+	private float lastCloneTime = 0f;
+	// The locator of where to create the clone.
+	private Transform target;
+
+
+	void Awake()
+	{
+		target = transform.Find("spawnLocation");
+	}
+
+	public bool IsReadyToClone()
+	{
+		return Time.time > lastCloneTime + cooldownTime;
+	}
+
+	void OnTriggerEnter2D(Collider2D originator)
+	{
+		if (IsReadyToClone())
+		{
+			Instantiate(archetype, target.position, target.rotation);
+			lastCloneTime = Time.time;
+		}
+	}
+}
